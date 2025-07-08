566af27ff69b9cfa4fc80181d1098e31b7180189
NasimSoury <nasim.soury69@gmail.com>
2025-07-08 13:12:33 +0200

    add dic

---
+++ Basic/dictionary_example.py
@@ -0,0 +1,14 @@
+nomre={
+    "jadi": {
+        "riazi": 13 ,
+        "farsi": 10 ,
+        "zaban": 2 ,
+        "shimi": 17 ,
+    } ,
+    "sara": {
+        "riazi": 14 ,
+        "farsi": 18 ,
+        "zaban": 19 ,
+        "shimi": 20 ,
+    }
+       }
