3c17a211eed8e5db2d8e9052345310fe1c935acd
NasimSoury <nasim.soury69@gmail.com>
2025-07-08 13:21:42 +0200

    edit_dict

--- Basic/dictionary_example.py
+++ Basic/dictionary_example.py
@@ -1,14 +1,18 @@
-nomre={
+scores= {
     "jadi": {
         "riazi": 13 ,
+        "programming": 20 ,
         "farsi": 10 ,
         "zaban": 2 ,
         "shimi": 17 ,
     } ,
     "sara": {
         "riazi": 14 ,
+        "programming": 4 ,
         "farsi": 18 ,
         "zaban": 19 ,
         "shimi": 20 ,
     }
        }
+print(scores["jadi"]["zaban"])
+print(scores["sara"]["riazi"])
