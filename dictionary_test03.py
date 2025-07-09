5e95ab78291b26a4de662ae8f081a635b87bb0f9
NasimSoury <nasim.soury69@gmail.com>
2025-07-09 14:39:44 +0200

    dictionary_test03.py

---
+++ dictionary_test03.py
@@ -0,0 +1,18 @@
+scores={
+    "sami" : 16,
+    "sara" : 18 ,
+    "nona" : 19 ,
+    "jadi" : 20 ,
+    "jan" : 15 
+}
+print(f"list students : " , scores)
+
+scores["sara"]= 4
+del scores["jan"]
+scores["monika"]= 15
+print(scores)
+for name , score in scores.items():
+    if score <10 :
+        print(f"{name} : you are failed")
+    else:
+        print(f"{name} : you are passed")
