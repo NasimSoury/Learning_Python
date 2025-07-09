d568c4d39d375df21523fb48edfc4fc793a74634
NasimSoury <nasim.soury69@gmail.com>
2025-07-09 13:50:31 +0200

    Update Average.py

--- Basic/Average.py
+++ Basic/Average.py
@@ -1,5 +1,5 @@
-num1=float(input("add your number:"))
-num2=float(input("add your number:"))
+num1=float(input("add your first number:"))
+num2=float(input("add your second number:"))
 sum_result=num1+num2
 print(sum_result)
 avrage=sum_result / 2
