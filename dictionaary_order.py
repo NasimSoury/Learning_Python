2a1b6b3ff59efd01d79f576d9a5ff15d3969f7d8
NasimSoury <nasim.soury69@gmail.com>
2025-07-10 11:56:56 +0200

    dictionary_order.py

---
+++ dictionaary_order.py
@@ -0,0 +1,12 @@
+orders={
+    "foods":["pizza" , "cola" , "salad"] ,
+}
+orders["order_id"]= 1025
+orders["name"]= "sara"
+#both ways of adding ara correct.
+
+print(orders)
+
+orders["status"]= "preparing"
+orders["total"] = 19.5
+print(orders)
