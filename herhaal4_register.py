1ddab436ccea77501b80d5d6c8c3545b2facc366
NasimSoury <nasim.soury69@gmail.com>
2025-08-19 13:00:39 +0200

    herhaal4_registeration.py

---
+++ herhaal4_register.py
@@ -0,0 +1,7 @@
+name=input("what is your name?:")
+city=input("add your city:")
+age=int(input("how old are you?:"))
+if age <13 :
+    print("you cant register!")
+else:
+    print("your register is successfull")
