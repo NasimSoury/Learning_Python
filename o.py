f252b4a6549d8aa32d02d6ff51a5d34f533608ec
NasimSoury <nasim.soury69@gmail.com>
2026-05-23 12:14:34 +0200

    moruur1.py

---
+++ o.py
@@ -0,0 +1,10 @@
+n=int(input("add your number : "))
+if n %3 ==0:
+    print("majik")
+elif n %5 == 0:
+    print("nefrin shode")
+
+elif n%3==0 and n%5==0 :
+    print("afsaneei")
+else :
+    print("normal")
