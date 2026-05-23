59bea60788fc9a2695dd0e927a0ab85d23707d53
NasimSoury <nasim.soury69@gmail.com>
2026-05-23 16:35:39 +0200

    moruur3.py

---
+++ moruur3.py
@@ -0,0 +1,9 @@
+#میانگین سه عدد
+num1=float(input("voeg eeste getal toe: "))
+num2=float(input("voeg tweede getal toe: "))
+num3=float(input("voeg derde getal toe: "))
+average=num1+num2+num3
+result=average/3
+print("average is ", result)
+# یا حرفه ای تر
+#print("average is", (num1 + num2 + num3) / 3)
