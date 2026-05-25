61d0fb9f0da142d493483da030432c5fb3da5ee8
NasimSoury <nasim.soury69@gmail.com>
2026-05-25 15:23:33 +0200

    een text omgekeerd doen

--- morur/moruur8.py
+++ morur/moruur8.py
@@ -1,8 +1,10 @@
+#برعکس کردن رشته
+
 string="nahid"
 print(string[::-1]) 
 
 #of in het allgemeen:
 text=input("voeg een naam:")
 omgekeerd=text[: : -1]
-print(omgekeerd)
+print(omgekeerd) #print(text[::-1])
 
